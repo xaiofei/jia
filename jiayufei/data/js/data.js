23ad780e7f9553b98b0f2b8b388325a8a731657b
xiaofei <1373878042@qq.com>
2017-05-15 10:35:44 +0800

    zuoye

--- jiayufei/data/js/data.js
+++ jiayufei/data/js/data.js
@@ -17,7 +17,29 @@ exports.data = function () {
                 random.string('lower', 4);
                 random.date('yyy-MM-dd');
                 var data = mock.mock({
-                    "menuList|2": [{
+                    'menuNavContent|1-7': [{
+                        'hao': '@integer(0,10)',
+                        'xi': "iphone"+"@integer(0,10)",
+                        'price': "@integer(2000,5000)"
+                    }]
+                })
+                //res.write();
+                res.end(JSON.stringify(data));
+            }
+        },
+        {
+            route: '/render',
+            handle: function (req, res, next) {
+                res.writeHead(200, {
+                    "Content-type": "application/json;charset=utf-8",
+                    "Access-Control-Allow-Origin": "*"
+                });
+                var random = mock.Random;
+                random.integer();
+                random.string('lower', 4);
+                random.date('yyy-MM-dd');
+                var data = mock.mock({
+                    "menuList|5": [{
                         'menuNav': '@string',
                         'menuNavContent|1-5': [{
                             'url': 'index.html',
--- jiayufei/gulpfile.js
+++ jiayufei/gulpfile.js
@@ -1,27 +1,28 @@
 var gulp = require('gulp');
-var uglify = require("gulp-uglify");
+var uglify = require("gulp-uglify");//js压缩
 var concat = require("gulp-concat");//文件合并
-var cssmin = require("gulp-clean-css");
+var cssmin = require("gulp-clean-css");//css压缩
 var sass = require("gulp-sass");//sacc编译
 var webserver = require('gulp-webserver');//web服务热启动
 var browserify = require('gulp-browserify');//模块化的打包
-var image = require('gulp-imagemin');
-var rev = require('gulp-rev');
-var revcoll = require('gulp-rev-collector');
+var image = require('gulp-imagemin');//图片压缩
+var rev = require('gulp-rev');//- 对文件名加MD5后缀
+var revcoll = require('gulp-rev-collector');//- 路径替换
+//var notify = require('gulp-notify');//提示信息
 var url = require('url');
 var datajson = require('./data/js/data.js');
 gulp.task('fei', function () {
     gulp.src('src/js/*.js')
-        .pipe(concat("all.js"))
+        //.pipe(concat("all.js"))
         .pipe(uglify())
-        .pipe(rev())
+        //.pipe(rev())
         //.pipe(browserify({
         //    insertGlobals : true,
         //    debug : !gulp.env.production
         //}))
         .pipe(gulp.dest('./build/js'))
-        .pipe(rev.manifest())
-        .pipe(gulp.dest('./rev/js'))
+    //.pipe(rev.manifest())
+    //.pipe(gulp.dest('./rev/js'))
 });
 
 gulp.task('ss', function () {
@@ -39,8 +40,7 @@ gulp.task('htmlrev', function () {
         .pipe(revcoll({
             replaceReved: true,
             dirReplacements: {
-                'css': 'css',
-                'js': 'js'
+                'css': 'css'
             }
         }))
         .pipe(gulp.dest('./build/html'));
@@ -77,6 +77,6 @@ gulp.task("webserver", ["build"], function () {
                 })
                 next();
             },
-            open: "./html/index.html"
+            open: "./html/filter.html"
         }));
 });
